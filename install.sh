7098aec92d9ac622995dbd3f0d356bdf033cbfa5
Caleb Smith <karagenit@outlook.com>
2018-06-12 22:16:55 -0400

    Add Manpage to Install/Uninstall Scripts
    Closes #6

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 cp -i termdiff.sh /usr/local/bin/termdiff
+cp -i termdiff.1 /usr/local/man/man1/termdiff.1
--- uninstall.sh
+++ uninstall.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 rm -i /usr/local/bin/termdiff
+rm -i /usr/local/man/man1/termdiff.1
